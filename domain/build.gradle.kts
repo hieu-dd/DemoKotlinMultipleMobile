e9b02b7693e6950e794c7d90f2d10e2e48452936
14H <doduchieu.kstn@gmail.com>
2022-04-09 14:09:03 +0700

    + Fix bug Mac M1

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -10,8 +10,7 @@ version = "1.0"
 
 kotlin {
     android()
-    iosX64()
-    iosArm64()
+    ios()
     iosSimulatorArm64()
 
     cocoapods {
@@ -48,23 +47,16 @@ kotlin {
         }
         val androidMain by getting
         val androidTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
         val iosMain by getting {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+            
         }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by getting {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
+        val iosTest by getting
+
+        val iosSimulatorArm64Main by getting {
+            dependsOn(iosMain)
+        }
+        val iosSimulatorArm64Test by getting {
+            dependsOn(iosTest)
         }
     }
 }
